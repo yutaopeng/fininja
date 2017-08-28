476735927066852b0383a6bd82a2333d348b0dd3
Yutao Peng <ypeng@financialengines.com>
2017-08-28 09:28:11 -0700

    add images

--- ts/Data/Images.ts
+++ ts/Data/Images.ts
@@ -1,11 +1,14 @@
 class Images {
-
+    public static Wheel: string = 'wheel';
+    public static Pin: string = 'pin';
     /**
      * A list of all images we need to show the preloader itself.
      * These should be loaded in the splash screen.
      */
     public static preloadList: string[] = [
         //Add images for the preloader
+        Images.Wheel,
+        Images.Pin
     ];
 
     /**
@@ -13,5 +16,7 @@ class Images {
      */
     public static list: string[] = [
         //Add images to load
+        Images.Wheel,
+        Images.Pin
     ];
 }
